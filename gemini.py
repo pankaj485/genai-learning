4d8f9a7152683bef493287caea64dd8e2d75d6c5
pankaj485 <pankajsamar2@gmail.com>
2025-10-29 22:12:22 +0530

    add request with gemini

---
+++ gemini.py
@@ -0,0 +1,13 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Hello There",
+)
+
+print(response.text)
