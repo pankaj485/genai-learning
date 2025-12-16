60373c2998d49476dafccc41bd62dfc005fb2a67
pankaj485 <pankajsamar2@gmail.com>
2025-12-16 15:20:21 +0530

    add pdf reader and text splitter with langchan

--- rag/main.py
+++ rag/main.py
@@ -1,10 +1,17 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # download from https://www.anuragkapur.com/assets/blog/programming/node/PDF-Guide-Node-Andrew-Mead-v3.pdf and save it as  "node-guide.pdf"
 pdf_file_path = Path(__file__).parent / "node-guide.pdf"
 
+# parses the pdf file and loads texts in the program
 loader = PyPDFLoader(pdf_file_path)
+documents = loader.load()
 
-# parses the pdf file and loads
-print(loader.load())
+
+# split the document into smaller text chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+text_chunks = text_splitter.split_documents(documents)
+
+print(text_chunks)
